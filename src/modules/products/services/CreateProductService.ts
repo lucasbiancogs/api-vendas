93f855574f535b43863ad4f470a1ac39b0e04188
Lucas Bianco <lucas.biancogs@hotmail.com>
2021-02-07 13:38:13 -0300

    Creating services

---
+++ src/modules/products/services/CreateProductService.ts
@@ -0,0 +1,35 @@
+import { getCustomRepository } from 'typeorm';
+import AppError from '../../../shared/errors/appError';
+import Product from '../typeorm/entities/Product';
+import { ProductsRepository } from '../typeorm/repositories/ProductsRepository';
+
+interface IRequest {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class CreateProductService {
+  public async execute({ name, price, quantity }: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductsRepository);
+
+    const productExists = await productsRepository.findByName(name);
+
+    if (productExists) {
+      throw new AppError('A product with this name already exists');
+    }
+
+    // Esse método somente cria o objeto, por isso é síncrono
+    const product = productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await productsRepository.save(product);
+
+    return product;
+  }
+}
+
+export default CreateProductService;
---
+++ src/modules/products/services/ListProductService.ts
@@ -0,0 +1,16 @@
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+import { ProductsRepository } from '../typeorm/repositories/ProductsRepository';
+
+class ListProductService {
+  public async execute(): Promise<Product[]> {
+    const productsRepository = getCustomRepository(ProductsRepository);
+
+    const products: Product[] = await productsRepository.find();
+
+
+    return products;
+  }
+}
+
+export default ListProductService;
