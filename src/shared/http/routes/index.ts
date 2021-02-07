a7461031183648f4fda940b0fb48c462ab971c56
Lucas Bianco <lucas.biancogs@hotmail.com>
2021-02-07 15:27:54 -0300

    Creating routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
+import productsRouter from '../../../modules/products/routes/products.routes';
 
 const routes = Router();
+
+routes.use('/products', productsRouter);
 
 routes.get('/', (request, response) => {
     return response.json({ message: 'Hello Dev' });
 });
+
+
 
 export default routes;
