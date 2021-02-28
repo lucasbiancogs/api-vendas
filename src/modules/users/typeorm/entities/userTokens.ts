9d5bcd7274bb82a29897ac40cc6ff0a8fa08314a
Lucas Bianco <lucas.biancogs@hotmail.com>
2021-02-28 16:56:21 -0300

    Implementando a tabela de userTokens

---
+++ src/modules/users/typeorm/entities/userTokens.ts
@@ -0,0 +1,22 @@
+import { Column, CreateDateColumn, Entity, Generated, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('user_tokens')
+class UserTokens {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  @Generated('uuid')
+  token: string;
+
+  @Column({ name: 'user_id' })
+  userId: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
+
+export default UserTokens;
